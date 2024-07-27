f0b4adfaf6bf4f2267a8227e9faf6483722544d1
kevinvilla01 <kevin01villa0101@gmail.com>
2024-07-26 22:16:18 -0600

    Creacion de variable session para login

--- Web/sesion.php
+++ Web/sesion.php
@@ -6,5 +6,20 @@ session_start();
 $user = $_POST['username'];
 $pw = $_POST['password'];
 
+$query = ("SELECT * FROM USUARIOS WHERE username='$user' AND passwd='$pw'");
+
+$consulta = pg_query($conection, $query);
+$cantidad = pg_num_rows($consulta);
+
+if($cantidad > 0){
+
+    $_SESSION['username']=$user;
+    header('Location:ingreso.php');
+
+}
+
+else{
+    echo "Datos incorrectos";
+}
 
 ?>
