5229a7c0eae883052c0a15ebf6a16a923d8a6d47
kevinvilla01 <kevin01villa0101@gmail.com>
2024-08-08 17:10:05 -0600

    creacion de archivo php y eliminacion de otro

--- inventario/php/connection.php
+++ inventario/php/connection.php
@@ -18,8 +18,8 @@ class Connection
             $connection = new Connection();
             $connection->connect = new PDO("{$connection->driver}:host={$connection->host};port={$connection->port};dbname={$connection->dbname}", $connection->user, $connection->password);
             $connection->connect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            //return $connection->connect;
-            echo "Connection succes";
+            return $connection->connect;
+            //echo "Connection succes";
         } catch(PDOException $e) {
             echo "Error: " . $e->getMessage();
         }
